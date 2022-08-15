897c810400e9c1b6c125be4c379c68b423abb725
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-08-14 21:49:15 -0300

    (coreecion) agregado de tipo de dato a tb_cliente.telefono agregado de tamaño a los int

--- base-de-datos-facturacion.sql
+++ base-de-datos-facturacion.sql
@@ -3,17 +3,17 @@ CREATE DATABASE db_facturacion;
 USE db_facturacion;
 
 CREATE TABLE tb_cliente(
-  codigo int,
-  nit int,
+  codigo int (2),
+  nit int (2),
   nombre varchar (80),
-  telefono,
+  telefono int (2),
   direccion text,
   
   PRIMARY KEY (codigo)
 );
 
 CREATE TABLE tb_proveedor(
-  codigo int,
+  codigo int (2),
   proveedor varchar (60),
   telefono,
   direccion text,
@@ -22,9 +22,9 @@ CREATE TABLE tb_proveedor(
 );
 
 CREATE TABLE tb_producto(
-  codigo int,
+  codigo int (2),
   descripcion varchar (100),
-  proveedor int UNIQUE,
+  proveedor int (2) UNIQUE,
   precio_costo decimal,
   precio_venta decimal,
   foto text,
@@ -34,9 +34,9 @@ CREATE TABLE tb_producto(
 );
 
 CREATE TABLE tb_factura(
-  numero int,
+  numero int (2),
   fecha datetime,
-  codigo_cliente int UNIQUE,
+  codigo_cliente int (2) UNIQUE,
   total_factura decimal,
   
   PRIMARY KEY (numero),
@@ -44,9 +44,9 @@ CREATE TABLE tb_factura(
 );
 
 CREATE TABLE tb_detalle_factura(
-  id int AUTO_INCREMENT,
-  numero_factura int UNIQUE,
-  codigo_producto int UNIQUE,
+  id int (2) AUTO_INCREMENT,
+  numero_factura int (2) UNIQUE,
+  codigo_producto int (2) UNIQUE,
   cantidad_pedida decimal,
   precio_total decimal,
   
