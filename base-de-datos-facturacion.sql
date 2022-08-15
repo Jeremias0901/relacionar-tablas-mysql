92df61aa41817dd35617a2fc087b071d65f84305
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-08-14 21:17:25 -0300

    creacion de la tabla tb_cliente

--- base-de-datos-facturacion.sql
+++ base-de-datos-facturacion.sql
@@ -2,4 +2,12 @@ CREATE DATABASE db_facturacion;
 
 USE db_facturacion;
 
-
+CREATE TABLE tb_cliente(
+  codigo int,
+  nit,
+  nombre varchar (80),
+  telefono,
+  direccion text,
+  
+  PRIMARY KEY (codigo)
+);
